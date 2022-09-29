7d0e972874964c045d9f3d21222409acc46c0264
zeel27 <110594630+zeel27@users.noreply.github.com>
2022-09-29 11:46:55 +1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -36,19 +36,19 @@ function draw() {
   
    drawSprites();
    
- var select_sprites = Math.round(random(1,3));
-
   
 
-   if (frameCount % 80 == 0) {
-     if (select_sprites == 1) {
-       createApples();
-     } else if (select_sprites == 2) {
-       createOrange();
-     }
-  }
+var select_sprites = Math.round(random(1,3));
 
-  
+if (frameCount % 80 = 0) {
+  if (select_sprites == 1) {
+   createApples();
+    } else if (select_sprites == 2) {
+      createOrange();
+    }else {
+     createRed();
+    }
+   }
 
 
 
